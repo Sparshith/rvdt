872ea7e98d44e9d87f02410ea87ee9e5db2db3cd
Sparshith Sampath <sparshith@Sparshiths-MacBook-Pro.local>
2016-09-20 17:13:17 +0530

    .htaccess changes

--- includes/header.php
+++ includes/header.php
@@ -8,8 +8,8 @@ $url = $_SERVER['REQUEST_URI'];
         <a class="toc item ln30">
           <i class="sidebar icon"></i>
         </a>
-        <a href="/index.php" class="item ln30 <?php echo ($url == '/' || $url == '' || $url == '/index.php') ? 'active': ''; ?>">Home</a>
-        <a href="/matchups.php" class="item ln30 <?php echo ($url == '/matchups.php') ? 'active': ''; ?>">Matchups</a>
+        <a href="/index" class="item ln30 <?php echo ($url == '/' || $url == '' || $url == '/index') ? 'active': ''; ?>">Home</a>
+        <a href="/matchups" class="item ln30 <?php echo ($url == '/matchups') ? 'active': ''; ?>">Matchups</a>
         <a href="/teams" class="item ln30">Teams</a>
         <a href="/judges" class="item ln30">Judges</a>
       <div class="right item">
