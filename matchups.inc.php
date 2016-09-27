b3770fee09b671254ffea032517afce9fa10eda8
Sparshith Sampath <sparshithsampath@gmail.com>
2016-09-27 17:22:11 +0530

    adding round id and venue logic

--- matchups.inc.php
+++ matchups.inc.php
@@ -4,18 +4,27 @@ include 'includes/application_top.php';
 $_title = 'Matchups | RVDT 2016';
 $DB = new DBPDO();
 
+$round_id = isset($_GET['round_id']) && $_GET['round_id'] ? $_GET['round_id'] : 1;
 
-$matchups_raw_data = $DB->fetchAll("SELECT * FROM `matchups` WHERE round_id = 1 ORDER BY match_id, position_id");
+$matchups_raw_data = $DB->fetchAll("SELECT * FROM `matchups` WHERE round_id = ? ORDER BY match_id, position_id", array($round_id));
 $teams_raw_data =  $DB->fetchAll("SELECT * FROM `teams`");
+$venues_raw_data =  $DB->fetchAll("SELECT * FROM `venues`");
+
 
 $teams = array();
 foreach ($teams_raw_data as $data) {
   $teams[$data['team_id']] = $data['team_name'];
 }
 
+$venues = array();
+foreach ($venues_raw_data as $data) {
+  $venues[$data['venue_id']] = $data['venue_name'];
+}
+
 $matchups = array();
 foreach ($matchups_raw_data as $data) {
   $matchups[$data['match_id']][$data['position_id']] = $data['team_id']; 
+  $matchups[$data['match_id']]['venue_id'] = $data['venue_id'];
 }
 
 
@@ -25,7 +34,7 @@ $table_contents = '';
 foreach($matchups as $matchup) {
   $table_contents .= '
      <tr>
-        <td>Venue</td>
+        <td>'.$venues[$matchup['venue_id']].'</td>
         <td>'.$teams[$matchup[1]].'</td>
         <td>'.$teams[$matchup[2]].'</td>
         <td>'.$teams[$matchup[3]].'</td>
