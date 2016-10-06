dd7f2d91553c362155d849a934302f8e12831720
Sparshith Sampath <sparshithsampath@gmail.com>
2016-10-06 22:15:04 +0530

    adding logic for team-wise matchusp

---
+++ team.php
@@ -0,0 +1,15 @@
+<?php include 'team.inc.php'; ?>
+<html>
+<?php include 'meta.php'; ?>
+<body>
+<?php include 'includes/header.php'; ?>
+<div class="wrapper">
+ 	<div class="pusher">
+	 	<div class="ui large header <?php echo $user_device == 'mobile' ? 'ml10i' : ''; ?>" style="margin-top: 30px;">
+	 		<?php echo 'Matchups for Team: '. $team_name; ?>
+	 	</div>
+ 		<?php echo $matchups_content; ?>
+ 	</div>
+</div>
+</body>
+</html>
