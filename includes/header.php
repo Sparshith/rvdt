b780ea32f2c7ca5dee24b5262e3870e550e5d7e0
Sparshith Sampath <sparshithsampath@gmail.com>
2016-10-07 11:31:44 +0530

    add header code for mobile

--- includes/header.php
+++ includes/header.php
@@ -5,14 +5,14 @@ $url = isset(parse_url($_SERVER['REQUEST_URI'])['path']) ?  parse_url($_SERVER['
 ?>
 <div class="ui container rv-header <?php echo ($user_device != 'mobile')  ? 'mbot10' : ''; ?>">
     <div class="ui large secondary inverted pointing menu wrapper">
-        <a class="toc item ln30">
+        <a class="toc item ln30" style="margin-bottom: 5px;">
           <i class="sidebar icon"></i>
         </a>
         <a href="/" style="border-left:1px solid #4d4d4d" class="item ln30 <?php echo ($url == '/' || $url == '' || $url == '/index') ? 'current-page': ''; ?>">Home</a>
         <a href="/matchups" class="item ln30 <?php echo ($url == '/matchups') ? 'current-page': ''; ?>">Matchups</a>
         <a href="/teams" class="item ln30">Teams</a>
         <a href="/judges" style="border-right:1px solid #4d4d4d" class="item ln30">Judges</a>
-      <div class="right item" style="padding-right:0px">
+      <div class="right item" style="<?php echo $user_device == 'mobile' ? 'padding-right:10px' : 'padding-right:0px'; ?>">
         <div class=mr10>
           <a class="ui inverted button">Contact</a>
         </div>
